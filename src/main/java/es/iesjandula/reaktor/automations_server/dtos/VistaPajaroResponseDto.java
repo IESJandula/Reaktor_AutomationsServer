74bf0e4a327024d1cb2f31a9dab5c987e97f7e36
Paco Benitez Chico <paco.benitez.chico@gmail.com>
2026-01-26 12:48:11 +0100

    añadir getter y setters

--- src/main/java/es/iesjandula/reaktor/automations_server/dtos/VistaPajaroResponseDto.java
+++ src/main/java/es/iesjandula/reaktor/automations_server/dtos/VistaPajaroResponseDto.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class VistaPajaroResponseDto
 {
 	private Map<String, List<ActuadorResponseDto>> mapaActuadores ;
