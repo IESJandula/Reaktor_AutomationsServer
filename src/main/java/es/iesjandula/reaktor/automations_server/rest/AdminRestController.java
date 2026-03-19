1fcd63a4896617a59d720a798362b3cca1fc06e8
adriLunaDelMoral <adrian.luna.del.moral@gmail.com>
2026-03-19 19:40:16 +0100

    Cambio en el orden de las acciones, las ultimas se muestran las primeras

--- src/main/java/es/iesjandula/reaktor/automations_server/rest/AdminRestController.java
+++ src/main/java/es/iesjandula/reaktor/automations_server/rest/AdminRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -885,7 +886,7 @@ public class AdminRestController
 	{
 		try
 		{
-			 Pageable pageable = PageRequest.of(page, size);
+			Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
 		     return ResponseEntity.ok(this.accionRepository.buscarAccionesPagina(pageable));
 		}	
 	    catch (Exception exception)
