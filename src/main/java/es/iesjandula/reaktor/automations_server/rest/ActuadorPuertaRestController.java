2e94d413ec4b8d8688ce0d0f50a17e39f45b14d6
Paco Benitez Chico <paco.benitez.chico@gmail.com>
2026-04-17 15:49:55 +0200

    corrección de bug

--- src/main/java/es/iesjandula/reaktor/automations_server/rest/ActuadorPuertaRestController.java
+++ src/main/java/es/iesjandula/reaktor/automations_server/rest/ActuadorPuertaRestController.java
@@ -69,8 +69,8 @@ public class ActuadorPuertaRestController
 		{
 			if (actuadorPuertaRequestDto.getMac() == null || actuadorPuertaRequestDto.getMac().isEmpty())
 			{
-				log.error(Constants.ERR_ACTUADOR_NULO_VACIO);
-				throw new AutomationsServerException(Constants.ERR_ACTUADOR_NULO_VACIO, Constants.ERR_ACTUADOR_CODE);
+				log.error(Constants.ERR_ACTUADOR_MAC_NULO_VACIO);
+				throw new AutomationsServerException(Constants.ERR_ACTUADOR_CODE, Constants.ERR_ACTUADOR_NULO_VACIO);
 			}
 
 			String mac = actuadorPuertaRequestDto.getMac();
